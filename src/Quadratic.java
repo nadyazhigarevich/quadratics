f5c61f5c50dc970e3c48b61497b0d2ffc49c54ad
nadyazhigarevich <zhigarevichnadya@gmail.com>
2025-01-30 22:50:27 +0300

    first commit

---
+++ src/Quadratic.java
@@ -0,0 +1,39 @@
+public class Quadratic {
+    private int a;
+    private int b;
+    private int c;
+
+    public Quadratic(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Quadratic() {
+
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+}
